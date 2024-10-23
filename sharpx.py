63dfd0916717ed1c57a0a6042b16e9e091608ca3
contentai417 <vsjsjjvsjsje@gmail.com>
2024-10-23 21:44:31 +0530

    Add files via upload

---
+++ sharpx.py
@@ -0,0 +1,17 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY SharpX72
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("8157026267:AAF0rQ6pQcOOiKFfIfK8oOE0TRlhR9GVbB4")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("SharpX72")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("5388393253")
+
+
+
+
+
+  
